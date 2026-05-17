7aacfd8883bd173449d7ae9d7fdb815a4fa28e07
Pravalika Matteda <pravalika.matteda0001@gmail.com>
2026-05-17 12:36:49 +0530

    Added Print messages within LoginTest

--- src/test/java/com/creatio/crm/application/tests/LoginTest.java
+++ src/test/java/com/creatio/crm/application/tests/LoginTest.java
@@ -17,6 +17,7 @@ public class LoginTest extends BaseTest{
 	{
 		System.out.println("entered 'Login' - method");
 		loginActions.loginProcess();
+		System.out.println("finished 'Login Process");
 		loginActions.logOutProcess();
 	}
 
